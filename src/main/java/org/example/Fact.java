b1c61cec3dd8217ae9c239308e1255b9a20d74be
ilk07 <ilk07@inbox.ru>
2022-12-19 19:30:28 +0300

    initial commit

---
+++ src/main/java/org/example/Fact.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Fact {
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final Integer upvotes;
+
+
+    public Fact(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") int upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId(){return id;}
+    public String getText(){return text;}
+    public String getType(){return type;}
+    public String getUser(){return user;}
+    public Integer getUpvotes(){return upvotes;}
+
+    @Override
+    public String toString() {
+        return "Fact: " +
+                "\n id = " + id +
+                "\n text = " + text +
+                "\n type = " + type +
+                "\n user = " + user +
+                "\n upvotes = " + upvotes;
+    }
+}
